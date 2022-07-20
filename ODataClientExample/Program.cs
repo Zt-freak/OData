11efcc5fd854d5b544e932b1bb24898237bde0ec
Apodemus Erectus <bnkho98@hotmail.com>
2022-07-21 00:06:10 +0200

    initial commit

---
+++ ODataClientExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OData.SampleService.Models.TripPin;
+
+namespace ODataClientExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListPeople().Wait();
+        }
+
+        static async Task ListPeople()
+        {
+            var serviceRoot = "https://services.odata.org/V4/TripPinServiceRW/";
+            var context = new DefaultContainer(new Uri(serviceRoot));
+
+            IEnumerable<Person> people = await context.People.ExecuteAsync();
+            foreach (var person in people)
+            {
+                Console.WriteLine("{0} {1}", person.FirstName, person.LastName);
+            }
+        }
+    }
+}
